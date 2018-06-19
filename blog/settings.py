8c20ad382c9d5fcb6423d1587ee9875c957f07cd
Chris Zielinski <ckz8780@gmail.com>
2018-06-19 01:23:38 +0000

    Fixed settings.py database setup

--- blog/settings.py
+++ blog/settings.py
@@ -80,17 +80,17 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-if os.environ.get('development'):
+    
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
 
