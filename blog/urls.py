c6b55e336a66b503c2de4a60738509a58882e9d5
Chris Zielinski <ckz8780@gmail.com>
2018-06-19 00:41:10 +0000

    Updated media urls and readme

--- blog/urls.py
+++ blog/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 from django.views.generic import RedirectView
-from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='posts/')),
     path('posts/', include('posts.urls')),
-    path('media/<path>.*', serve, {'document_root': MEDIA_ROOT}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
